4f10e2cb4e9f3f2d3715932e8a33947572563175
Subhadip Bag <85691735+subhadip07@users.noreply.github.com>
2024-07-25 23:38:02 +0530

    Updated Largest in Array code

---
+++ Arrays/largestInArray.java
@@ -0,0 +1,29 @@
+public class largestInArray
+{
+    public static int getLargest(int numbers[])
+    {
+        int largest=Integer.MIN_VALUE;
+        int smallest=Integer.MAX_VALUE;
+        for(int i=0; i < numbers.length; i++)
+        {
+            if(largest < numbers[i])
+            {
+                largest=numbers[i];
+            }
+            if(smallest > numbers[i])
+            {
+                smallest=numbers[i];
+            }
+        }
+        System.out.println("Smallest value is: " + smallest);
+        System.out.println("Largest value is: " + largest);
+        return largest;    
+    }
+
+    public static void main(String args[])
+    {
+        int numbers[]={1,2,6,3,5};
+        System.out.println("Largest value is: " + getLargest(numbers));
+        
+    }
+}
