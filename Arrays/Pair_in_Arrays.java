8e37d7955427d67996f26aa0cb0b8c2d9db00e8d
Subhadip Bag <85691735+subhadip07@users.noreply.github.com>
2024-07-29 23:03:27 +0530

    Added Pair in Array program

---
+++ Arrays/Pair_in_Arrays.java
@@ -0,0 +1,23 @@
+public class Pair_in_Arrays 
+{
+    public static void printpairs(int numbers[])
+    {
+        int tp=0;
+        for(int i=0; i<numbers.length; i++)
+        {
+            int curr=numbers[i];
+            for(int j=i+1; j<numbers.length; j++)
+            {
+                System.out.println("(" + curr+ ", " + numbers[j] + ")");
+                tp++;
+            }
+            System.out.println();
+        }
+        System.out.println("Total pairs: " + tp);
+    }
+    public static void main(String args[])
+    {
+        int numbers[]={2,4,6,8,10};
+        printpairs(numbers);
+    }    
+}
