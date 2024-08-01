7df6715489972edc37028cb139c9de9bca46496a
Subhadip Bag <85691735+subhadip07@users.noreply.github.com>
2024-08-01 22:30:15 +0530

    Added Maximum Subarray Sum Using Prefix Sum method

---
+++ Arrays/Max_Subarray_Sum2.java
@@ -0,0 +1,38 @@
+import java.util.*;
+public class Max_Subarray_Sum2 
+{
+    public static void maxSubarraySum2(int numbers[])
+    {
+        int currSum=0;
+        int maxSum=Integer.MIN_VALUE;
+        int prefix[]=new int[numbers.length];
+
+        prefix[0]=numbers[0];
+        //Calculate Prefix Array
+        for(int i=1; i<prefix.length; i++)
+        {
+            prefix[i]=prefix[i-1] + numbers[i];
+        }
+
+        for(int i=0; i<numbers.length; i++)
+        {
+            int start=i;
+            for(int j=i; j<numbers.length; j++)
+            {
+                int end=j;
+                currSum=start==0? prefix[end]: prefix[end]-prefix[start-1];
+
+                if(maxSum < currSum)
+                {
+                    maxSum=currSum;
+                }
+            }
+        }
+        System.out.println("Max Sum = " + maxSum);
+    }    
+    public static void main(String args[])
+    {
+        int numbers[]={1,-3,6,-2,5};
+        maxSubarraySum2(numbers);
+    }
+}
