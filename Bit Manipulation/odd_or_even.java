d8ed5203000f86b70dd9f258dcb146942771ecbc
Subhadip Bag <85691735+subhadip07@users.noreply.github.com>
2024-08-11 22:16:51 +0530

    Update odd_or_even.java

--- Bit Manipulation/odd_or_even.java
+++ Bit Manipulation/odd_or_even.java
@@ -1,1 +1,23 @@
+public class odd_or_even
+{
+    public static void oddoreven(int n)
+    {
+        int bitmask=1;
+        if((n&bitmask)==0)
+        {
+            //even number
+            System.out.println("Even number");
+        }
+        else
+        {
+            System.out.println("Odd number");
+        }
+    }
 
+    public static void main(String args[])
+    {
+        oddoreven(3);
+        oddoreven(10);
+        oddoreven(17);
+    }
+}
