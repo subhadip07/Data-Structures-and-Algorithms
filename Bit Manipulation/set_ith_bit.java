8c82cba409df351229d134e860a7637c4f5bb6ed
Subhadip Bag <85691735+subhadip07@users.noreply.github.com>
2024-08-13 22:25:11 +0530

    Added Set ith Bit code

---
+++ Bit Manipulation/set_ith_bit.java
@@ -0,0 +1,13 @@
+public class set_ith_bit 
+{
+    public static int setIthBit(int n, int i)
+    {
+        int bitmask=1<<i;
+        return n | bitmask;
+    }
+
+    public static void main(String args[])
+    {
+        System.out.println(setIthBit(10, 2));
+    }
+}
