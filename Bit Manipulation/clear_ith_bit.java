21a5c32bc8af2d536fe439ca2ededb312aead390
Subhadip Bag <85691735+subhadip07@users.noreply.github.com>
2024-08-13 22:35:33 +0530

    Added Clear ith Bit code

---
+++ Bit Manipulation/clear_ith_bit.java
@@ -0,0 +1,13 @@
+public class clear_ith_bit 
+{
+    public static int clearIthBit(int n, int i)
+    {
+        int bitmask = ~(1<<i);
+        return n & bitmask;
+    }    
+
+    public static void main(String[] args) 
+    {
+        System.out.println(clearIthBit(10, 1));    
+    }
+}
