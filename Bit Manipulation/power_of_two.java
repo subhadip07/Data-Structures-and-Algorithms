675f8b2f98338faed702e7114e94d6e081770107
Subhadip Bag <85691735+subhadip07@users.noreply.github.com>
2024-08-15 19:44:46 +0530

    Added Whether given number is Power of Two code

---
+++ Bit Manipulation/power_of_two.java
@@ -0,0 +1,12 @@
+public class power_of_two 
+{
+    public static boolean isPowerOfTwo(int n)
+    {
+        return (n & (n-1))==0;
+    }    
+
+    public static void main(String[] args) 
+    {
+        System.out.println(isPowerOfTwo(15));    
+    }
+}
