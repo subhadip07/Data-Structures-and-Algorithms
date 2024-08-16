79c96425c7c2a0d6a60d12829d3dfb1c98180946
Subhadip Bag <85691735+subhadip07@users.noreply.github.com>
2024-08-16 19:41:50 +0530

    Added Count Set Bits in a Number code

---
+++ Bit Manipulation/count_set_bits.java
@@ -0,0 +1,21 @@
+public class count_set_bits 
+{
+    public static int countsetbits(int n)
+    {
+        int count=0;
+        while(n>0)
+        {
+            if((n & 1) != 0)
+            {//check our LSB
+                count++;
+            }
+            n=n>>1;
+        }
+        return count;
+    }    
+
+    public static void main(String[] args) 
+    {
+        System.out.println(countsetbits(10));
+    }
+}
