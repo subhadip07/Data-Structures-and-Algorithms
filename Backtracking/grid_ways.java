51dfd2d7677b0b2ca12f71849e0a523f0dcd8c5c
Subhadip Bag <85691735+subhadip07@users.noreply.github.com>
2024-09-01 23:25:37 +0530

    Added grid ways code

---
+++ Backtracking/grid_ways.java
@@ -0,0 +1,24 @@
+public class grid_ways 
+{
+    public static int gridways(int i, int j, int n, int m)
+    {
+        if(i==n-1 && j==m-1)
+        {// condition for last cell
+            return 1;
+        }
+        else if(i==n || j==m)
+        {//boundary cross condition
+            return 0;
+        }
+
+        int w1=gridways(i+1, j, n, m);
+        int w2=gridways(i, j+1, n, m);
+        return w1+w2;
+    }
+
+    public static void main(String[] args) 
+    {
+        int n=3, m=3;
+        System.out.println(gridways(0, 0, n, m));    
+    }
+}
